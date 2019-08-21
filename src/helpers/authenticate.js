d6a553476740dbb108e1bc20d4b102e9070d9606
Fikri Rahmat Nurhidayat <FikriRNurhidayat@gmail.com>
2019-08-21 10:19:42 +0700

    [Fikk] Remove console.log

--- src/helpers/authenticate.js
+++ src/helpers/authenticate.js
@@ -2,14 +2,12 @@ const jwt = require('jsonwebtoken');
 const { errorsResponse } = require('./responseFormatter.js');
 
 module.exports = function (req, res, next) {
-  console.log(req.headers);
   var headerToken = req.headers.authorization;
   if (!headerToken) {
     return res.status(401).json(errorsResponse('Denied!'));
   }
 
   try {
-    console.log(req.header.authorzation);
     req.headers.authorization = jwt.verify(headerToken.split(" ")[1], process.env.SECRET_OR_KEY); 
     next();
   }
