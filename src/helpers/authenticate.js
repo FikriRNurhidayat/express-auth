43823e5d8096079deed4272dac1e9999d79e85ec
Fikri Rahmat Nurhidayat <FikriRNurhidayat@gmail.com>
2019-08-27 11:23:44 +0700

    [Fikk] Mbuh

--- src/helpers/authenticate.js
+++ src/helpers/authenticate.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
 const { errorsResponse } = require('./responseFormatter.js');
+const User = require('../models/user.js');
 
-module.exports = function (req, res, next) {
+module.exports = async function (req, res, next) {
   var headerToken = req.headers.authorization;
   if (!headerToken) {
     return res.status(401).json(errorsResponse('Denied!'));
@@ -9,6 +10,9 @@ module.exports = function (req, res, next) {
 
   try {
     req.headers.authorization = jwt.verify(headerToken.split(" ")[1], process.env.SECRET_OR_KEY); 
+    let user = await User.findById(req.headers.authorization);
+    if (!user) return res.status(404).json(errorsResponse("User doesn't exist!"));
+
     next();
   }
 
